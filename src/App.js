4cec45625e583ff85b21fea3b439af4d3b7b38c1
JM <jmjusmin1412@gmail.com>
2022-10-29 02:27:58 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./styles.css";
 import UseRefExample1 from "./components/UseRefExample1";
 import UseRefExample2 from "./components/UseRefExample2";
+import UseRefExample3 from "./components/UseRefExample3";
 
 export default function App() {
   return (
     <div className="App">
-      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 }
